806e8e678496b1f9e57cf6c8f382d25675444273
Chris Manson <chris@manson.ie>
2015-06-29 10:13:19 +0100

    making CRUD work the way I want it to

--- index.js
+++ index.js
@@ -1,47 +1,77 @@
-var authorisationFunction = require('./lib/authorisation');
-var executionFunction = require('./lib/execution');
-var findOne = require('./lib/findOne');
-var queryFunction = require('./lib/query');
-var sortFunction = require('./lib/sort');
-var selectFunction = require('./lib/select');
-var createFunction = require('./lib/create');
-var paginationFunction = require('./lib/pagination');
-var createExecution = require('./lib/createExecution');
-var identityMiddleware = require('./lib/identityMiddleware');
+var pluralize = require('pluralize');
 
-module.exports = function (options) {
+var authorisationFunction = require('./lib/authorisation');
+var createExecution = require('./lib/createExecution');
+var createFunction = require('./lib/create');
+var deleteOne = require('./lib/deleteOne');
+var error = require('./lib/error');
+var findMany= require('./lib/findMany');
+var findOne = require('./lib/findOne');
+var identityMiddleware = require('./lib/identityMiddleware');
+var paginationFunction = require('./lib/pagination');
+var queryFunction = require('./lib/query');
+var selectFunction = require('./lib/select');
+var sortFunction = require('./lib/sort');
+var updateOne = require('./lib/updateOne');
+
+module.exports = function(options) {
 
     //check required fields
-    if (!options.model) throw new Error("Mongoose model is missing");
+    if (!options.model) {
+        throw new Error("Mongoose model is missing");
+    }
 
     //set the defaults
     var resource = options.resource || options.model.collection.name;
-    var query = {};
 
     var outputJson = {};
 
     if (options.find) {
         outputJson.get = {};
-        outputJson.get["/" + resource] = [
-            options.find.authentication || identityMiddleware,
+        outputJson.get["/" + pluralize(resource)] = [
+            options.authentication || identityMiddleware,
             authorisationFunction(options),
             queryFunction(options),
             paginationFunction(options),
             sortFunction(options),
             selectFunction(options),
-            executionFunction(options)
+            findMany(options),
+            error
         ];
-        outputJson.get["/" + resource + "/:id"] = [
-            options.find.authentication || identityMiddleware,
+        outputJson.get["/" + pluralize(resource) + "/:id"] = [
+            options.authentication || identityMiddleware,
             authorisationFunction(options),
-            findOne(options)
+            findOne(options),
+            error
         ];
     }
-    if(options.create){
+    if (options.create) {
         outputJson.post = {};
-        outputJson.post["/" + resource] = [
+        outputJson.post["/" + pluralize(resource)] = [
+            options.authentication || identityMiddleware,
             createFunction(options),
-            createExecution(options)
+            createExecution(options),
+            error
+        ];
+    }
+
+    if (options.update) {
+        outputJson.put = {};
+        outputJson.put["/" + pluralize(resource) + "/:id"] = [
+            options.authentication || identityMiddleware,
+            authorisationFunction(options),
+            updateOne(options),
+            error
+        ];
+    }
+
+    if (options.delete) {
+        outputJson.delete = {};
+        outputJson.delete["/" + pluralize(resource) + "/:id"] = [
+            options.authentication || identityMiddleware,
+            authorisationFunction(options),
+            deleteOne(options),
+            error
         ];
     }
 
