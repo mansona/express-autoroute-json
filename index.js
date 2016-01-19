51611fcc0e5beb7c3bdc7d75b5f6bfe2f0a650e8
Chris Manson <chris@manson.ie>
2016-01-19 11:49:06 +0000

    adding local pre middleware blocks

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var createFunction = require('./lib/create');
 var deleteOne = require('./lib/deleteOne');
 var emptyMiddleware = require('./lib/emptyMiddleware');
 var error = require('./lib/error');
-var findMany= require('./lib/findMany');
+var findMany = require('./lib/findMany');
 var findOne = require('./lib/findOne');
 var identityMiddleware = require('./lib/identityMiddleware');
 var paginationFunction = require('./lib/pagination');
@@ -18,20 +18,21 @@ var updateOne = require('./lib/updateOne');
 
 module.exports = function(options) {
 
-    //check required fields
-    if (!options.model) {
-        throw new Error("Mongoose model is missing");
-    }
+  //check required fields
+  if (!options.model) {
+    throw new Error('Mongoose model is missing');
+  }
 
-    //set the defaults
-    var resource = options.resource || options.model.collection.name;
+  //set the defaults
+  var resource = options.resource || options.model.collection.name;
 
-    var outputJson = {};
+  var outputJson = {};
 
-    if (options.find) {
-        outputJson.get = {};
-        outputJson.get["/" + pluralize(resource)] = [
+  if (options.find) {
+    outputJson.get = {};
+    outputJson.get['/' + pluralize(resource)] = [
             options.preMiddleware || identityMiddleware,
+            _.get(options, 'find.preMiddleware', identityMiddleware),
             options.authentication || identityMiddleware,
             _.get(options, 'find.authentication', identityMiddleware),
             authorisationFunction(options),
@@ -40,59 +41,65 @@ module.exports = function(options) {
             sortFunction(options),
             selectFunction(options),
             findMany(options),
-            error
+            error,
         ];
-        outputJson.get["/" + pluralize(resource) + "/:id"] = [
+
+    outputJson.get['/' + pluralize(resource) + '/:id'] = [
             options.preMiddleware || identityMiddleware,
+            _.get(options, 'find.preMiddleware', identityMiddleware),
             options.authentication || identityMiddleware,
             _.get(options, 'find.authentication', identityMiddleware),
             authorisationFunction(options),
             findOne(options),
-            error
+            error,
         ];
-    }
-    if (options.create) {
-        outputJson.post = {};
-        outputJson.post["/" + pluralize(resource)] = [
+  }
+
+  if (options.create) {
+    outputJson.post = {};
+    outputJson.post['/' + pluralize(resource)] = [
             options.preMiddleware || identityMiddleware,
+            _.get(options, 'create.preMiddleware', identityMiddleware),
             options.authentication || identityMiddleware,
             _.get(options, 'create.authentication', identityMiddleware),
             createFunction(options),
             createExecution(options),
             _.get(options, 'create.postMiddleware', emptyMiddleware),
-            error
+            error,
         ];
-    }
+  }
 
-    if (options.update) {
-        outputJson.put = {};
-        outputJson.put["/" + pluralize(resource) + "/:id"] = [
+  if (options.update) {
+    outputJson.put = {};
+    outputJson.put['/' + pluralize(resource) + '/:id'] = [
             options.preMiddleware || identityMiddleware,
+            _.get(options, 'update.preMiddleware', identityMiddleware),
             options.authentication || identityMiddleware,
             _.get(options, 'update.authentication', identityMiddleware),
             authorisationFunction(options),
             updateOne(options),
             _.get(options, 'update.postMiddleware', emptyMiddleware),
-            error
+            error,
         ];
-    }
+  }
 
-    if (options.delete) {
-        outputJson.delete = {};
-        outputJson.delete["/" + pluralize(resource) + "/:id"] = [
+  if (options.delete) {
+    outputJson.delete = {};
+    outputJson.delete['/' + pluralize(resource) + '/:id'] = [
             options.preMiddleware || identityMiddleware,
+            _.get(options, 'delete.preMiddleware', identityMiddleware),
             options.authentication || identityMiddleware,
             _.get(options, 'delete.authentication', identityMiddleware),
             authorisationFunction(options),
             deleteOne(options),
             _.get(options, 'delete.postMiddleware', emptyMiddleware),
-            error
+            error,
         ];
-    }
+  }
 
-    if (options.debug) {
-        console.log(outputJson);
-    }
+  if (options.debug) {
+    console.log(outputJson);
+  }
 
-    return outputJson;
+  return outputJson;
 };
