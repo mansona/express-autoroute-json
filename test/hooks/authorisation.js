ffbe1cc8babd17f6db883be969b1b5db80c985ea
Chris Manson <chris@stonecircle.io>
2018-02-05 21:32:56 +0000

    starting local block definition tests

--- test/hooks/authorisation.js
+++ test/hooks/authorisation.js
@@ -9,6 +9,47 @@ var fixture = require('../fixtures/loadData');
 
 var Chat = require('../models/chat')();
 
+function successfulDelete(id) {
+  return Chat.findOne({
+    count: id,
+  }).exec().then(function(chat) {
+    return request(global.app).delete('/chats/' + chat._id).expect(204);
+  }).then(() => {
+    // check that it deleted
+    return Chat.findOne({
+      count: id,
+    }).then((chat) => {
+      expect(chat).to.be.null;
+    });
+  });
+}
+
+function successfulUpdate(id) {
+  return Chat.findOne({
+    count: id,
+  }).exec().then(function(chat) {
+    return request(global.app)
+      .patch('/chats/' + chat._id)
+      .set('Accept', 'application/json')
+      .send({
+        data: {
+          attributes: {
+            name: 'super awesome new name',
+          },
+          type: 'chats',
+        },
+      })
+      .expect(200);
+  }).then(() => {
+    // check that it deleted
+    return Chat.findOne({
+      count: id,
+    }).then((chat) => {
+      expect(chat).to.have.property('name', 'super awesome new name');
+    });
+  });
+}
+
 describe.only('the authorisation hook', function() {
   beforeEach(function() {
     // reset app
@@ -59,18 +100,7 @@ describe.only('the authorisation hook', function() {
 
     describe('delete', () => {
       it('should allow you to delete authorised items', () => {
-        return Chat.findOne({
-          count: 6,
-        }).exec().then(function(chat) {
-          return request(global.app).delete('/chats/' + chat._id).expect(204);
-        }).then(() => {
-          // check that it deleted
-          return Chat.findOne({
-            count: 6,
-          }).then((chat) => {
-            expect(chat).to.be.null;
-          });
-        });
+        return successfulDelete(6);
       });
 
       it('should fail if you try to delete unauthorised object', () => {
@@ -91,29 +121,7 @@ describe.only('the authorisation hook', function() {
 
     describe('update', () => {
       it('should allow you to update authorised items', () => {
-        return Chat.findOne({
-          count: 6,
-        }).exec().then(function(chat) {
-          return request(global.app)
-            .patch('/chats/' + chat._id)
-            .set('Accept', 'application/json')
-            .send({
-              data: {
-                attributes: {
-                  name: 'super awesome new name',
-                },
-                type: 'chats',
-              },
-            })
-            .expect(200);
-        }).then(() => {
-          // check that it deleted
-          return Chat.findOne({
-            count: 6,
-          }).then((chat) => {
-            expect(chat).to.have.property('name', 'super awesome new name');
-          });
-        });
+        return successfulUpdate(6);
       });
 
       it('should fail if you try to update unauthorised object', () => {
@@ -145,10 +153,53 @@ describe.only('the authorisation hook', function() {
   });
 
   describe('local block definition', () => {
+    beforeEach(() => {
+      global.app.use(bodyParser.json());
+      autoroute(global.app, {
+        throwErrors: true,
+        routesDir: path.join(process.cwd(), 'test', 'fixtures', 'scenarios', 'authorisation', 'global'),
+      });
+    });
+
     describe('find', () => {
-      it('should return the right number of items based on authorisation');
-      it('should allow for direct access of authorised object');
-      it('should fail on access of unauthorised object');
+      it('should return the right number of items based on authorisation', (done) => {
+        request(global.app)
+          .get('/chats')
+          .expect(function(res) {
+            expect(_.size(res.body.data)).to.equal(6);
+          })
+          .end(global.jsonAPIVerify(done));
+      });
+      it('should allow for direct access of authorised object', () => {
+        return Chat.findOne({
+          count: 6,
+        }).exec().then(function(chat) {
+          return request(global.app).get('/chats/' + chat._id).expect(200);
+        });
+      });
+      it('should fail on access of unauthorised object', () => {
+        return Chat.findOne({
+          count: 8,
+        }).exec().then(function(chat) {
+          return request(global.app).get('/chats/' + chat._id).expect(404);
+        });
+      });
+
+      describe('should not affect other routes', () => {
+        // describe('create'); // TODO ???
+        describe('delete', () => {
+          it('should allow you to delete clearly authorised chat', () => {
+            return successfulDelete(6);
+          });
+          it('should allow you to delete chat that does not pass the other block authorisation', () => {
+            return successfulDelete(8);
+          });
+        });
+        describe('update', () => {
+          it('should allow you to update clearly authorised chat');
+          it('should allow you to update chat that does not pass the other block authorisation');
+        });
+      });
     });
 
     describe('create', () => {
